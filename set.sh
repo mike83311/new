6fe0e620dcfd62e0f501e0ed5a8c4d48d786bc5b
mike83311 <mike83311@gmail.com>
2015-12-09 11:02:07 +0800

    Create set.sh

---
+++ set.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+~~~~~UPDATE~~~~~
+sudo apt-get update
+echo "DONE!"
+
+~~~~~UPGRADE~~~~~
+sudo apt-get upgrade -y
+echo "DONE!"
+
+~~~~~VIM~~~~~
+sudo apt-get install vim -y
+echo "DONE!"
+
+
+
+
+
+
+
+
